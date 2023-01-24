005684961c18c432a64ff10882a067a43af1d5c7
Imran <imransh11@gmail.com>
2023-01-24 18:59:26 -0500

    not finished

--- data.js
+++ data.js
@@ -1,1 +1,16 @@
-// Your code here
+// Your code here.
+window.addEventListener('DOMContentLoaded', (e) => {
+    document.getElementById('add').addEventListener('click', e => {
+        let newItem = document.createElement('li')
+
+        newItem.setAttribute('data-type', data.type)
+        newItem.dataType = newItem.getAttribute('type')
+
+        let ul = document.querySelector('#shopping-list')
+
+
+
+        ul.append(newItem)
+    })
+})
+// console.log(ul,'ull')
