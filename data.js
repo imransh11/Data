68538eecc4754272de58104176d9a0a4951a242c
lola <keepthechange2021@gmail.com>
2023-01-24 16:24:11 -0800

    will's note

--- data.js
+++ data.js
@@ -1,16 +1,38 @@
 // Your code here.
-window.addEventListener('DOMContentLoaded', (e) => {
-    document.getElementById('add').addEventListener('click', e => {
-        let newItem = document.createElement('li')
+// window.addEventListener('DOMContentLoaded', (e) => {
+//     document.getElementById('add').addEventListener('click', e => {
+//         let newItem = document.createElement('li')
 
-        newItem.setAttribute('data-type', data.type)
-        newItem.dataType = newItem.getAttribute('type')
+//         newItem.setAttribute('data-type', data.type)
+//         newItem.dataType = newItem.getAttribute('type')
 
-        let ul = document.querySelector('#shopping-list')
+//         let ul = document.querySelector('#shopping-list')
 
 
 
-        ul.append(newItem)
-    })
-})
+//         ul.append(newItem)
+//     })
+// })
 // console.log(ul,'ull')
+
+function addItem(e) {
+    //prevent default behavior of the form submission
+    e.preventDefault();
+    const shoppingList = document.getElementById('shopping-List');
+    const name = document.getElementById('name')
+    const type = document.getElementById('type')
+
+    const li = document.createElement("li")
+    li.innerText = name.value;
+    li.dataset.type = type.value;
+    shoppingList.appendChild(li);
+    name.value = "";
+    type.value = "dairy";
+
+
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+    document.getElementById("add")
+    .addEventListener("click", addItem);
+});
